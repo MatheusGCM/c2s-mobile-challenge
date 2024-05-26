761ecf424840dcfa7c2765ed9b4dd54e46bb96b3
Matheus <matheusgcm10@outlook.com>
2024-05-26 11:24:24 -0300

    feat: initial setup app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_700Bold as Roboto700,
+  Roboto_500Medium as Roboto500,
+  Roboto_400Regular as Roboto400,
+} from '@expo-google-fonts/roboto'
+import React from 'react'
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+
+import { Home } from '@/screens/Home'
+import { GlobalStyle } from '@/styles/global'
+import { defaultTheme } from '@/styles/themes'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Roboto700,
+    Roboto500,
+    Roboto400,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle>
+        <StatusBar />
+        <Home />
+      </GlobalStyle>
+    </ThemeProvider>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const GlobalStyle = styled.View`
+  flex: 1;
+  padding: 20px 10px;
+  background-color: ${({ theme }) => theme.background};
+`
